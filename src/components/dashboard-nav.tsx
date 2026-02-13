90e82e45c5725fd8be3e7024a51356452672885b
Cristian Ruben <cristianruben.developer@gmail.com>
2026-02-13 15:25:07 -0600

    change header

--- src/components/dashboard-nav.tsx
+++ src/components/dashboard-nav.tsx
@@ -16,7 +16,7 @@ export function DashboardNav({ user }: { user: User }) {
 	const searchParams = useSearchParams();
 	const [searchQuery, setSearchQuery] = useState(
 		searchParams.get("search") || "",
-	);
+	); 
 
 	// Debounce search update
 	useEffect(() => {
@@ -39,7 +39,27 @@ export function DashboardNav({ user }: { user: User }) {
 
 	return (
 		<>
-			<nav className="w-full">
+			<nav className="bg-white border-b border-gray-200 w-full z-50">
+				<div className="max-w-7xl mx-auto px-4 flex items-center justify-between gap-4 py-2">
+					<Link href="/dashboard" className="flex items-center gap-4">
+						<SystemLogo />
+					</Link>
+
+					<div className="flex items-center gap-4">
+						<h2 className="text-sm text-primary font-bold">METRA</h2>
+						<div className="w-px h-10 bg-primary" />
+						<div>
+							<p className="text-xs text-primary font-bold leading-tight max-w-[280px]">
+								MONITOREO, ESTADISTICA TENDENCIA Y
+							</p>
+							<p className="text-xs text-primary font-bold leading-tight max-w-[280px]">
+								REPORTES PARA EL ANALISIS DE LA DLPC
+							</p>
+						</div>
+					</div>
+				</div>
+			</nav>
+			<nav className="w-full border-b border-gray-200">
 				<section className="max-w-7xl mx-auto px-4 flex items-center justify-between gap-4 py-4">
 					<div className="flex-1 max-w-md">
 						{isReportPage ? (
@@ -72,27 +92,6 @@ export function DashboardNav({ user }: { user: User }) {
 						<UserDropdownMobile user={user} />
 					</div>
 				</section>
-			</nav>
-
-			<nav className="bg-white border-b border-gray-200 w-full z-50">
-				<div className="max-w-7xl mx-auto px-4 flex items-center justify-between gap-4 py-2">
-					<Link href="/dashboard" className="flex items-center gap-4">
-						<SystemLogo />
-					</Link>
-
-					<div className="flex items-center gap-4">
-						<h2 className="text-sm text-primary font-bold">METRA</h2>
-						<div className="w-px h-10 bg-primary" />
-						<div>
-							<p className="text-xs text-primary font-bold leading-tight max-w-[280px]">
-								MONITOREO, ESTADISTICA TENDENCIA Y
-							</p>
-							<p className="text-xs text-primary font-bold leading-tight max-w-[280px]">
-								REPORTES PARA EL ANALISIS DE LA DLPC
-							</p>
-						</div>
-					</div>
-				</div>
 			</nav>
 		</>
 	);
